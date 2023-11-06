408e494a29ae13279dd3c88d7ef63f71d02d529a
e1iguerron789 <130955581+e1iguerron789@users.noreply.github.com>
2023-11-06 05:16:02 -0500

    Create Producto.java

---
+++ Producto.java
@@ -0,0 +1,53 @@
+public class Producto {
+
+    public String nombreproducto;
+    private String tipo;
+
+    private int cantidadproducto;
+
+    private int cantidadminima;
+    private double precio;
+
+
+
+    public Producto(String nombreproducto, String tipo, int cantidadproducto , int cantidadminima, double precio) {
+        this.nombreproducto = nombreproducto;
+        this.tipo= tipo;
+        this.cantidadproducto = cantidadproducto;
+        this.cantidadminima = cantidadminima;
+        this.precio = precio;
+
+    }
+
+    public int abastecerProducto(int pcantidadproducto)
+    {
+        return cantidadproducto=cantidadproducto+pcantidadproducto;
+    }
+    public void cambiarProducto(String pnombreproducto, String ptipo, int pcantidadproducto , int pcantidadminima, double pprecio )
+    {
+       nombreproducto=pnombreproducto;
+       tipo=ptipo;
+       cantidadproducto=pcantidadproducto;
+       cantidadminima=pcantidadminima;
+       precio=pprecio;
+
+    }
+
+
+    @Override
+    public String toString() {
+        String p = "Nombre del Producto: " +
+                nombreproducto+
+                "\nTipo de Producto : Producto de "+ tipo +
+                "\nCantidad actual del producto en la tienda: " + cantidadproducto +
+                "\nCantidad mínima para abastecimiento del producto:" + cantidadminima +
+                "\nPrecio base de venta por unidad.: " + precio +
+                "\nPrecio final con IVA:";
+        return p;
+    }
+
+
+
+}
+
+
