1f4563342654cc18f9cb4e335656f2bc3dbe354f
e1iguerron789 <130955581+e1iguerron789@users.noreply.github.com>
2023-11-06 05:15:01 -0500

    este si es el main , ando dormida

--- main.java
+++ main.java
@@ -1,51 +1,361 @@
-public class Producto {
+import java.util.Scanner;
 
-    public String nombreproducto;
-    private String tipo;
+public class Main {
+    public static void main(String[] args) {
+        Producto p1 = new Producto("Lapiz","Papeleria",50,10,0.90);
+        Producto p2 = new Producto("Leche","Supermercado",30,10,1.00);
+        Producto p3 = new Producto("Paracetamol","Farmacia",20,10,0.30);
+        Producto p4 = new Producto("Mermelada","Supermercado",50,10,1.20);
+        Producto p5 = null;
+        int opc;
+        String nombreproducto,tipo;
+        double precio;
+            int cantidadproducto,cantidadminima;
+        Scanner sc = new Scanner(System.in);
 
-    private int cantidadproducto;
+        do{
+            System.out.println("1.- Visualizar la información de los productos");
+            System.out.println("2.- Vender un producto.");
+            System.out.println("3.- Abastecer la tienda con un producto");
+            System.out.println("4.- Cambiar un producto.");
+            System.out.println("5.- Calcular estadísticas de ventas.");
+            System.out.println("6.- Salir");
+            System.out.print("\nIngrese una opción: ");
+            opc = Integer.parseInt(sc.next());
+            switch (opc) {
+                case 1: {
 
-    private int cantidadminima;
-    private double precio;
+                        System.out.println("1.- Productos de Papeleria");
+                        System.out.println("2.- Productos de Farmacia");
+                        System.out.println("3.- Productos de Supermercado");
+                        System.out.print("\nIngrese una opción: ");
+                        opc = Integer.parseInt(sc.next());
+                        switch (opc) {
+                            case 1: {
+                                System.out.println("1.- Productos de Papeleria");
+                                System.out.println(p1.toString());
+
+                            }
+                            break;
+                            case 2: {
+                                System.out.println("2.- Productos de Farmacia");
+                                System.out.println(p3.toString());
+                            }
+                            break;
+                            case 3: {
+
+                                System.out.println("3.- Productos de Supermercado");
+                                System.out.println(p2.toString());
+                                System.out.println(p4.toString());
+                            }
+                            break;
+
+                            default:
+                                System.out.println("opción no válida");
+
+
+                        }
+
+
+                }
+                break;
+                case 2: {
+
+
+                }
+                break;
+                case 3: {
+                    System.out.println("ABASTECIMIENTO DE PRODUCTOS");
+                    System.out.println("1.- Agregar cantidad de productos");
+                    System.out.println("2.- Agregar un nuevo producto");
+                    System.out.print("\nIngrese una opción: ");
+                    opc = Integer.parseInt(sc.next());
+                    switch (opc) {
+                        case 1: {
+                            System.out.println("1.- Productos de Papeleria");
+                            System.out.println("2.- Productos de Farmacia");
+                            System.out.println("3.- Productos de Supermercado");
+                            System.out.print("\nIngrese una opción: ");
+                            opc = Integer.parseInt(sc.next());
+                            switch (opc) {
+                                case 1: {
+                                    System.out.println("Productos de Papeleria");
+                                    System.out.println(" 1._" + p1.nombreproducto);
+                                    System.out.print("\nIngrese una opción: ");
+                                    opc = Integer.parseInt(sc.next());
+
+                                    switch (opc) {
+                                        case 1: {
+
+                                            System.out.print("Ingrese la cantidad de nuevos productos que agregara: ");
+                                            cantidadproducto = Integer.parseInt(sc.next());
+
+                                            p1.abastecerProducto(cantidadproducto);
+
+
+                                        }break;
+                                    }
+                                }break;
+                                case 2: {
+                                    System.out.println("Productos de Farmacia");
+                                    System.out.println("1.- " + p3.nombreproducto);
+                                    System.out.print("Ingrese una opción: ");
+                                    opc = Integer.parseInt(sc.next());
+
+                                    switch (opc) {
+                                        case 1: {
+                                            System.out.print("Ingrese la cantidad de nuevos productos que agregara: ");
+                                            cantidadproducto = Integer.parseInt(sc.next());
+
+                                            p3.abastecerProducto(cantidadproducto);
+
+
+                                        }break;
+                                    }
+                                }break;
+
+                                case 3: {
+
+                                    System.out.println("\nProductos de Supermercado");
+                                    System.out.println("1._"+p2.nombreproducto);
+                                    System.out.println("2._"+p4.nombreproducto);
+                                    System.out.print("Ingrese una opción: ");
+                                    opc = Integer.parseInt(sc.next());
+
+                                    switch (opc) {
+                                        case 1: {
+
+                                            System.out.print("Ingrese la cantidad de nuevos productos que agregara: ");
+                                            cantidadproducto = Integer.parseInt(sc.next());
+
+                                            p2.abastecerProducto(cantidadproducto);
+
+
+                                        }break;
+                                        case 2: {
+
+                                            System.out.print("Ingrese la cantidad de nuevos productos que agregara: ");
+                                            cantidadproducto = Integer.parseInt(sc.next());
+
+                                            p4.abastecerProducto(cantidadproducto);
+
+
+                                        }break;
+
+
+                                    }
 
 
 
-    public Producto(String nombreproducto, String tipo, int cantidadproducto , int cantidadminima, double precio) {
-        this.nombreproducto = nombreproducto;
-        this.tipo= tipo;
-        this.cantidadproducto = cantidadproducto;
-        this.cantidadminima = cantidadminima;
-        this.precio = precio;
+                                }break;
+                            }
+
+                        }break;
+                        case 2: {
+                            System.out.print("Ingrese el nombre del producto: ");
+                            nombreproducto = String.valueOf(sc.next());
+
+                            System.out.print("Ingrese el tipo del producto: ");
+                            tipo = String.valueOf(sc.next());
+
+                            System.out.print("Ingrese la cantidad del producto: ");
+                            cantidadproducto = Integer.parseInt(sc.next());
+
+                            System.out.print("Ingrese la cantidad minima del producto para el abastecimiento: ");
+                            cantidadminima = Integer.parseInt(sc.next());
+
+                            System.out.print("Ingrese el precio del producto: ");
+                            precio = Double.parseDouble(sc.next());
+
+
+                            p5 = new Producto(nombreproducto, tipo, cantidadproducto, cantidadminima, precio);
+
+
+                        }break;
+
+
+                    }
+
+
+                }
+                break;
+                case 4: {
+
+                    System.out.println("1.- Productos de Papeleria");
+                    System.out.println("2.- Productos de Farmacia");
+                    System.out.println("3.- Productos de Supermercado");
+                    System.out.print("\nIngrese una opción: ");
+                    opc = Integer.parseInt(sc.next());
+                    switch (opc) {
+                        case 1: {
+                            System.out.println("Productos de Papeleria");
+                            System.out.println(" 1._" + p1.nombreproducto);
+                            System.out.print("\nIngrese una opción: ");
+                            opc = Integer.parseInt(sc.next());
+
+                            switch (opc) {
+                                case 1: {
+                                    System.out.print("Ingrese el nombre del producto: ");
+                                    nombreproducto = String.valueOf(sc.next());
+
+                                    System.out.print("Ingrese el tipo del producto: ");
+                                    tipo = String.valueOf(sc.next());
+
+                                    System.out.print("Ingrese la cantidad del producto: ");
+                                    cantidadproducto = Integer.parseInt(sc.next());
+
+                                    System.out.print("Ingrese la cantidad minima del producto para el abastecimiento: ");
+                                    cantidadminima = Integer.parseInt(sc.next());
+
+                                    System.out.print("Ingrese el precio del producto: ");
+                                    precio = Double.parseDouble(sc.next());
+
+
+                                    p1.cambiarProducto(nombreproducto, tipo, cantidadproducto, cantidadminima, precio);
+
+
+                                }break;
+                            }
+                        }break;
+                        case 2: {
+                            System.out.println("Productos de Farmacia");
+                            System.out.println("1.- " + p3.nombreproducto);
+                            System.out.print("Ingrese una opción: ");
+                            opc = Integer.parseInt(sc.next());
+
+                            switch (opc) {
+                                case 1: {
+                                    System.out.print("Ingrese el nombre del producto: ");
+                                    nombreproducto = String.valueOf(sc.next());
+
+                                    System.out.print("Ingrese el tipo del producto: ");
+                                    tipo = String.valueOf(sc.next());
+
+                                    System.out.print("Ingrese la cantidad del producto: ");
+                                    cantidadproducto = Integer.parseInt(sc.next());
+
+                                    System.out.print("Ingrese la cantidad minima del producto para el abastecimiento: ");
+                                    cantidadminima = Integer.parseInt(sc.next());
+
+                                    System.out.print("Ingrese el precio del producto: ");
+                                    precio = Double.parseDouble(sc.next());
+
+
+                                    p3.cambiarProducto(nombreproducto, tipo, cantidadproducto, cantidadminima, precio);
+
+
+                                }break;
+                            }
+                        }break;
+
+                            case 3: {
+
+                                System.out.println("\nProductos de Supermercado");
+                                System.out.println("1._"+p2.nombreproducto);
+                                System.out.println("2._"+p4.nombreproducto);
+                                System.out.print("Ingrese una opción: ");
+                                opc = Integer.parseInt(sc.next());
+
+                                switch (opc) {
+                                    case 1: {
+                                        System.out.print("Ingrese el nombre del producto: ");
+                                        nombreproducto = String.valueOf(sc.next());
+
+                                        System.out.print("Ingrese el tipo del producto: ");
+                                        tipo = String.valueOf(sc.next());
+
+                                        System.out.print("Ingrese la cantidad del producto: ");
+                                        cantidadproducto = Integer.parseInt(sc.next());
+
+                                        System.out.print("Ingrese la cantidad minima del producto para el abastecimiento: ");
+                                        cantidadminima = Integer.parseInt(sc.next());
+
+                                        System.out.print("Ingrese el precio del producto: ");
+                                        precio = Double.parseDouble(sc.next());
+
+
+                                        p2.cambiarProducto(nombreproducto, tipo, cantidadproducto, cantidadminima, precio);
+
+
+                                    }break;
+                                    case 2: {
+                                        System.out.print("Ingrese el nombre del producto: ");
+                                        nombreproducto = String.valueOf(sc.next());
+
+                                        System.out.print("Ingrese el tipo del producto: ");
+                                        tipo = String.valueOf(sc.next());
+
+                                        System.out.print("Ingrese la cantidad del producto: ");
+                                        cantidadproducto = Integer.parseInt(sc.next());
+
+                                        System.out.print("Ingrese la cantidad minima del producto para el abastecimiento: ");
+                                        cantidadminima = Integer.parseInt(sc.next());
+
+                                        System.out.print("Ingrese el precio del producto: ");
+                                        precio = Double.parseDouble(sc.next());
+
+
+                                        p4.cambiarProducto(nombreproducto, tipo, cantidadproducto, cantidadminima, precio);
+
+
+                                    }break;
+
+
+                                }
+
+
+
+                            }break;
+                    }
+                }break;
+
+                case 5:{
+
+                        System.out.println("1.- El producto más vendido.");
+                        System.out.println("2.- El producto menos vendido.");
+                        System.out.println("3.- La cantidad total de dinero obtenido por las ventas de la tienda.");
+                        System.out.println("4.- La cantidad de dinero promedio obtenido por unidad de producto vendido");
+                        System.out.print("\nIngrese una opción: ");
+                        opc = Integer.parseInt(sc.next());
+                        switch (opc) {
+                            case 1: {
+
+
+                            }
+                            break;
+                            case 2: {
+
+                            }
+                            break;
+                            case 3: {
+
+
+                            }
+                            break;
+                            case 4: {
+
+                            }
+                            break;
+
+                            default:
+                                System.out.println("opción no válida");
+
+                        }
+
+
+                }break;
+
+                case 6:{
+                    System.out.println("Gracias!");
+                }break;
+                default:
+                    System.out.println("opción no válida");
+
+            }
+
+
+        }while(opc!=10);
 
     }
-
-    public int abastecerProducto(int pcantidadproducto)
-    {
-        return cantidadproducto=cantidadproducto+pcantidadproducto;
-    }
-    public void cambiarProducto(String pnombreproducto, String ptipo, int pcantidadproducto , int pcantidadminima, double pprecio )
-    {
-       nombreproducto=pnombreproducto;
-       tipo=ptipo;
-       cantidadproducto=pcantidadproducto;
-       cantidadminima=pcantidadminima;
-       precio=pprecio;
-
-    }
-
-
-    @Override
-    public String toString() {
-        String p = "Nombre del Producto: " +
-                nombreproducto+
-                "\nTipo de Producto : Producto de "+ tipo +
-                "\nCantidad actual del producto en la tienda: " + cantidadproducto +
-                "\nCantidad mínima para abastecimiento del producto:" + cantidadminima +
-                "\nPrecio base de venta por unidad.: " + precio +
-                "\nPrecio final con IVA:";
-        return p;
-    }
-
-
 
 }
