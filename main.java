a857e6ef088e9d7629a5317a89bb660135ce3980
e1iguerron789 <130955581+e1iguerron789@users.noreply.github.com>
2023-11-06 14:12:33 -0500

    Update main.java

--- main.java
+++ main.java
@@ -311,36 +311,78 @@ public class Main {
 
                 case 5:{
 
-                        System.out.println("1.- El producto más vendido.");
-                        System.out.println("2.- El producto menos vendido.");
-                        System.out.println("3.- La cantidad total de dinero obtenido por las ventas de la tienda.");
-                        System.out.println("4.- La cantidad de dinero promedio obtenido por unidad de producto vendido");
-                        System.out.print("\nIngrese una opción: ");
-                        opc = Integer.parseInt(sc.next());
-                        switch (opc) {
-                            case 1: {
+                    System.out.println("1.- El producto más vendido.");
+                    System.out.println("2.- El producto menos vendido.");
+                    System.out.println("3.- La cantidad total de dinero obtenido por las ventas de la tienda.");
+                    System.out.println("4.- La cantidad de dinero promedio obtenido por unidad de producto vendido");
 
-
+                    System.out.print("\nIngrese una opción: ");
+                    opc = Integer.parseInt(sc.next());
+                    switch (opc) {
+                        case 1: {
+                            if(p1.getCantidadproducto() < p2.getCantidadproducto() && p1.getCantidadproducto() < p3.getCantidadproducto() && p1.getCantidadproducto() < p4.getCantidadproducto()){
+                                System.out.println("El producto mas vendido fue: "+ p1.getNombreproducto());
+                                System.out.println("Quedaron un total de: "+p1.getCantidadproducto() );
+                            } else if(p2.getCantidadproducto() < p1.getCantidadproducto() && p2.getCantidadproducto() < p3.getCantidadproducto() && p2.getCantidadproducto() < p4.getCantidadproducto()){
+                                System.out.println("El producto mas vendido fue: "+ p2.getNombreproducto());
+                                System.out.println("Quedaron un total de: "+p2.getCantidadproducto());
+                            } else if(p3.getCantidadproducto() < p1.getCantidadproducto() && p3.getCantidadproducto() < p2.getCantidadproducto() && p3.getCantidadproducto() < p4.getCantidadproducto()){
+                                System.out.println("El producto mas vendido fue: "+ p3.getNombreproducto());
+                                System.out.println("Quedaron un total de: "+p3.getCantidadproducto());
+                            }else if(p4.getCantidadproducto() < p1.getCantidadproducto() && p4.getCantidadproducto() < p2.getCantidadproducto() && p4.getCantidadproducto() < p3.getCantidadproducto()){
+                                System.out.println("El producto mas vendido fue: "+ p4.getNombreproducto());
+                                System.out.println("Quedaron un total de: "+p4.getCantidadproducto());
+                            }else{
+                                System.out.println("No hubieron ventas");
                             }
-                            break;
-                            case 2: {
 
-                            }
-                            break;
-                            case 3: {
-
-
-                            }
-                            break;
-                            case 4: {
-
-                            }
-                            break;
-
-                            default:
-                                System.out.println("opción no válida");
 
                         }
+                        break;
+                        case 2: {
+
+                            if(p1.getCantidadproducto() > p2.getCantidadproducto() && p1.getCantidadproducto() > p3.getCantidadproducto() && p1.getCantidadproducto() > p4.getCantidadproducto()){
+                                System.out.println("El producto menos vendido fue: "+ p1.getNombreproducto());
+                                System.out.println("quedaron un total de: "+p1.getCantidadproducto() );
+                            } else if(p2.getCantidadproducto() > p1.getCantidadproducto() && p2.getCantidadproducto() > p3.getCantidadproducto() && p2.getCantidadproducto() > p4.getCantidadproducto()){
+                                System.out.println("El producto menos vendido fue: "+ p2.getNombreproducto());
+                                System.out.println("quedaron un total de: "+p2.getCantidadproducto() );
+                            } else if(p3.getCantidadproducto() > p1.getCantidadproducto() && p3.getCantidadproducto() > p2.getCantidadproducto() && p3.getCantidadproducto() > p4.getCantidadproducto()){
+                                System.out.println("El producto menos vendido fue: "+ p3.getNombreproducto());
+                                System.out.println("Quedaron un total de: "+p3.getCantidadproducto() );
+                            }else if(p4.getCantidadproducto() > p1.getCantidadproducto() && p4.getCantidadproducto() > p2.getCantidadproducto() && p4.getCantidadproducto() > p3.getCantidadproducto()){
+                                System.out.println("El producto menos vendido fue: "+ p4.getNombreproducto());
+                                System.out.println("Quedaron un total de: "+p4.getCantidadproducto() );
+                            }else{
+                                System.out.println("No hubieron ventas");
+                            }
+
+                        }
+                        break;
+                        case 3: {
+
+                            double dineroTotal;
+
+                            dineroTotal = p1.dineroObtenido() + p2.dineroObtenido() + p3.dineroObtenido() + p4.dineroObtenido();
+                            System.out.println("El dinero total obtenido de ventas es: "+ dineroTotal+ "$");
+
+
+                        }
+                        break;
+                        case 4: {
+
+                            System.out.println(p1.toStringPromedio());
+                            System.out.println(p2.toStringPromedio());
+                            System.out.println(p3.toStringPromedio());
+                            System.out.println(p4.toStringPromedio());
+
+                        }
+                        break;
+
+                        default:
+                            System.out.println("opción no válida");
+
+                    }
 
 
                 }break;
@@ -359,3 +401,5 @@ public class Main {
     }
 
 }
+
+   
